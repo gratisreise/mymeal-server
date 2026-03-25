617bd0f75091acd9221524818c4cb92eaf41169a
Jungho Kim <jeonghogim318@gmail.com>
2026-03-25 09:11:27 +0900

    refactor(settings): DTO 팩토리 메서드 패턴 적용 및 로그 한글화
    - SettingsResponse.from() 팩토리 메서드 추가
    - SettingsService에서 팩토리 메서드 사용하도록 리팩토링
    - 로그 메시지를 한국어로 변경하여 디버깅 편의성 향상
    - import 문 정리 및 Swagger 어노테이션 추가

--- src/main/java/com/mymealserver/api/settings/SettingsController.java
+++ src/main/java/com/mymealserver/api/settings/SettingsController.java
@@ -5,17 +5,24 @@ import com.mymealserver.api.settings.dto.response.SettingsResponse;
 import com.mymealserver.api.settings.service.SettingsService;
 import com.mymealserver.common.annotation.CurrentMember;
 import com.mymealserver.common.response.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/settings")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/settings")
 public class SettingsController {
 
     private final SettingsService settingsService;
--- src/main/java/com/mymealserver/api/settings/service/SettingsService.java
+++ src/main/java/com/mymealserver/api/settings/service/SettingsService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -20,77 +21,71 @@ public class SettingsService {
     private final MemberSettingsWriter memberSettingsWriter;
 
     public SettingsResponse getSettings(Long memberId) {
-        log.debug("Getting settings for memberId: {}", memberId);
+        log.debug("회원 설정 조회 시작: memberId={}", memberId);
 
         MemberSettings settings = memberSettingsReader.findByMemberId(memberId);
 
-        return new SettingsResponse(
-                new SettingsResponse.NotificationsResponse(
-                        settings.getRecommendationEnabled(),
-                        settings.getReactionReminderEnabled(),
-                        settings.getMealReminderEnabled(),
-                        new SettingsResponse.MealTimesResponse(
-                                settings.getBreakfastTime(),
-                                settings.getLunchTime(),
-                                settings.getDinnerTime()
-                        )
-                )
-        );
+        log.debug("회원 설정 조회 완료: memberId={}", memberId);
+        return SettingsResponse.from(settings);
     }
 
     @Transactional
     public void updateNotificationSettings(Long memberId, UpdateNotificationRequest request) {
-        log.debug("Updating notification settings for memberId: {}", memberId);
+        log.debug("알림 설정 업데이트 시작: memberId={}", memberId);
 
         MemberSettings settings = memberSettingsReader.findByMemberId(memberId);
 
+        // 추천 알림 설정 업데이트
         if (request.recommendationEnabled() != null) {
             if (request.recommendationEnabled()) {
                 settings.enableRecommendation();
             } else {
                 settings.disableRecommendation();
             }
-            log.debug("Updated recommendationEnabled to: {}", request.recommendationEnabled());
+            log.debug("추천 알림 설정 변경: recommendationEnabled={}", request.recommendationEnabled());
         }
 
+        // 반응 리마인더 알림 설정 업데이트
         if (request.reactionReminderEnabled() != null) {
             if (request.reactionReminderEnabled()) {
                 settings.enableReactionReminder();
             } else {
                 settings.disableReactionReminder();
             }
-            log.debug("Updated reactionReminderEnabled to: {}", request.reactionReminderEnabled());
+            log.debug("반응 리마인더 설정 변경: reactionReminderEnabled={}", request.reactionReminderEnabled());
         }
 
+        // 식사 리마인더 알림 설정 업데이트
         if (request.mealReminderEnabled() != null) {
             if (request.mealReminderEnabled()) {
                 settings.enableMealReminder();
             } else {
                 settings.disableMealReminder();
             }
-            log.debug("Updated mealReminderEnabled to: {}", request.mealReminderEnabled());
+            log.debug("식사 리마인더 설정 변경: mealReminderEnabled={}", request.mealReminderEnabled());
         }
 
+        // 식사 시간 설정 업데이트
         if (request.mealTimes() != null) {
             UpdateNotificationRequest.MealTimesData mealTimes = request.mealTimes();
 
             if (mealTimes.breakfast() != null) {
                 settings.setBreakfastTime(mealTimes.breakfast());
-                log.debug("Updated breakfastTime to: {}", mealTimes.breakfast());
+                log.debug("아침 식사 시간 변경: breakfastTime={}", mealTimes.breakfast());
             }
 
             if (mealTimes.lunch() != null) {
                 settings.setLunchTime(mealTimes.lunch());
-                log.debug("Updated lunchTime to: {}", mealTimes.lunch());
+                log.debug("점심 식사 시간 변경: lunchTime={}", mealTimes.lunch());
             }
 
             if (mealTimes.dinner() != null) {
                 settings.setDinnerTime(mealTimes.dinner());
-                log.debug("Updated dinnerTime to: {}", mealTimes.dinner());
+                log.debug("저녁 식사 시간 변경: dinnerTime={}", mealTimes.dinner());
             }
         }
 
         memberSettingsWriter.save(settings);
-        log.info("Updated notification settings for memberId: {}", memberId);
+        log.info("알림 설정 업데이트 완료: memberId={}", memberId);
     }
 }
