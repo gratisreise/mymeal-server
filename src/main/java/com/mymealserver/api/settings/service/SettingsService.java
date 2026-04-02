0b793637e87902c5e144564936301c93efe1d73c
Jungho Kim <jeonghogim318@gmail.com>
2026-04-02 10:20:35 +0900

    style: 코드 스타일 통일

--- src/main/java/com/mymealserver/api/settings/service/SettingsService.java
+++ src/main/java/com/mymealserver/api/settings/service/SettingsService.java
@@ -20,17 +20,12 @@ public class SettingsService {
   private final MemberSettingsWriter memberSettingsWriter;
 
   public SettingsResponse getSettings(Long memberId) {
-    log.debug("회원 설정 조회 시작: memberId={}", memberId);
-
     MemberSettings settings = memberSettingsReader.findByMemberId(memberId);
-
-    log.debug("회원 설정 조회 완료: memberId={}", memberId);
     return SettingsResponse.from(settings);
   }
 
   @Transactional
   public void updateNotificationSettings(Long memberId, UpdateNotificationRequest request) {
-    log.debug("알림 설정 업데이트 시작: memberId={}", memberId);
 
     MemberSettings settings = memberSettingsReader.findByMemberId(memberId);
 
@@ -41,7 +36,6 @@ public class SettingsService {
       } else {
         settings.disableRecommendation();
       }
-      log.debug("추천 알림 설정 변경: recommendationEnabled={}", request.recommendationEnabled());
     }
 
     // 반응 리마인더 알림 설정 업데이트
@@ -51,7 +45,6 @@ public class SettingsService {
       } else {
         settings.disableReactionReminder();
       }
-      log.debug("반응 리마인더 설정 변경: reactionReminderEnabled={}", request.reactionReminderEnabled());
     }
 
     // 식사 리마인더 알림 설정 업데이트
@@ -61,7 +54,6 @@ public class SettingsService {
       } else {
         settings.disableMealReminder();
       }
-      log.debug("식사 리마인더 설정 변경: mealReminderEnabled={}", request.mealReminderEnabled());
     }
 
     // 식사 시간 설정 업데이트
@@ -70,21 +62,17 @@ public class SettingsService {
 
       if (mealTimes.breakfast() != null) {
         settings.setBreakfastTime(mealTimes.breakfast());
-        log.debug("아침 식사 시간 변경: breakfastTime={}", mealTimes.breakfast());
       }
 
       if (mealTimes.lunch() != null) {
         settings.setLunchTime(mealTimes.lunch());
-        log.debug("점심 식사 시간 변경: lunchTime={}", mealTimes.lunch());
       }
 
       if (mealTimes.dinner() != null) {
         settings.setDinnerTime(mealTimes.dinner());
-        log.debug("저녁 식사 시간 변경: dinnerTime={}", mealTimes.dinner());
       }
     }
 
     memberSettingsWriter.save(settings);
-    log.info("알림 설정 업데이트 완료: memberId={}", memberId);
   }
 }
